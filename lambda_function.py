6189dae87399fd491404fa847d14508970420d8a
Vishnu Prasath <149139598+Vishnuprasath18@users.noreply.github.com>
2025-02-27 14:40:36 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,11 +1,24 @@
+import json
 import boto3
+import os
 
 s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
-    bucket_name = 'aws-project-source-bucket12'
-    file_name = 'test-image.jpg'
-    file_content = b'Hello, this is a test image'  # Replace with actual image bytes
+    # Extract bucket name and object key from the event
+    source_bucket = event['Records'][0]['s3']['bucket']['name']
+    object_key = event['Records'][0]['s3']['object']['key']
+    
+    # Destination bucket
+    destination_bucket = os.environ['DEST_BUCKET']
 
-    s3.put_object(Bucket=bucket_name, Key=file_name, Body=file_content)
-    print(f"{file_name} uploaded successfully to {bucket_name}")
+    try:
+        # Copy the object from source bucket to destination bucket
+        copy_source = {'Bucket': source_bucket, 'Key': object_key}
+        s3.copy_object(CopySource=copy_source, Bucket=destination_bucket, Key=object_key)
+        
+        print(f"Successfully copied {object_key} from {source_bucket} to {destination_bucket}")
+        
+    except Exception as e:
+        print(f"Error copying object: {str(e)}")
+        raise e
